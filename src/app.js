a2545cf66d300c8b0029a8d91f2caf63c7e09a52
nurRiyad <nur.riyad@fieldnation.com>
2025-12-21 00:08:29 +0600

    Add cart related api and error handler
    Signed-off-by: nurRiyad <nur.riyad@fieldnation.com>

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import categoryRoutes from './routes/categoryRoute.js';
 import productRoute from './routes/products/index.js';
+import cartRoute from './routes/cartRoute.js';
 
 const app = express();
 
@@ -11,11 +12,28 @@ app.use( express.json() );
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/category', categoryRoutes); 
-app.use('/product', productRoute); // /product 
+app.use('/product', productRoute);
+app.use('/cart', cartRoute);
+
+// handle not found routes
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: 'Not Found',
+        message: 'The requested resource was not found'
+    });
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        status: 'Error',
+        message: 'Something went wrong!'
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 
-// endpoint -> app.js -> router -> controller -> data-return
